6bc729b716d670f7166bd114dd2286c64b77a63b
Nicholas Santiago <nickjakesantiago@gmail.com>
2022-08-25 15:30:21 +0200

    completed mutate an array with const

--- es6.js
+++ es6.js
@@ -43,3 +43,18 @@ for (let l = 0; l < 3; l++) {
 console.log(printNumTwoBeta()); //displays l = 2
 console.log(l); // undefined
 // l is not defined because it was not declared in the global scope. It is only declared within the for loop statement. printNumTwoBeta() returned the correct value because three different l variables with unique values (0, 1, and 2) were created by the let keyword within the loop statement.
+
+//////////////////////////////////////////////
+// Mutate and array declared with const
+// Some developers prefer to assign all their variables using const by default, unless they know they will need to reassign the value. Only in that case, they use let.
+// However, it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration only prevents reassignment of the variable identifier.
+// this means we can change the values inside the array, however we cannpt redifine the entire array ie. change the value assigned to the variable identifier
+const s = [5, 6, 7];
+s = [1, 2, 3]; //cannot reassign variable identifier "s" to new array/value
+s[2] = 45; //we can however alter the existing values inside the array
+console.log(s); // logs array [5,6,45]
+// Like all arrays, the array elements in s are mutable, but because const was used, you cannot use the variable identifier s to point to a different array using the assignment operator.
+
+//////////////////////////////////////////////
+//Prevent Object Mutation
+//
